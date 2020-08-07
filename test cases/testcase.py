2e1c469883035adf9f682f8f164ff4152d260201
yishengdasheng <youyou.xu@enesource.com>
2020-08-07 17:26:25 +0800

    0807：update 元素定位

--- test cases/testcase.py
+++ test cases/testcase.py
@@ -16,6 +16,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from selenium.common import exceptions as e
+import time
 from config.read_conf import ReadConf
 from log.dolog import DoLog
 
@@ -55,7 +56,7 @@ class TestCases(unittest.TestCase):
 
         # 检查登录后首页的logo元素有没有出现
         # logo = driver.find_element_by_xpath('//p[@class="title ml30 flex-row flex-center"]')  # presence_of_element_located() 后面的值一定要是str
-        logo = '//p[@class="title ml30 flex-row flex-center"]'
+        logo = '//p[text()="能源节度使 "]'
         try:
             WebDriverWait(self.driver, 5, 0.5).until(expected_conditions.presence_of_element_located((By.XPATH, logo)))
         except e.InvalidSelectorException as error:
@@ -114,21 +115,40 @@ class TestCases(unittest.TestCase):
         # 切换到实时监测
         shishijiance_h1 = self.driver.find_element_by_xpath('//*[@id="app"]/div/div/div[1]/div[2]/ul/li[3]/div/span')
         shishigongkuang_h2 = self.driver.find_element_by_xpath('//*[@id="app"]/div/div/div[1]/div[2]/ul/li[3]/ul/li/ul/li[1]/span')
-        mingxichaxun_h2 = self.driver.find_element_by_xpath('//*[@id="app"]/div/div/div[1]/div[2]/ul/li[3]/ul/li/ul/li[2]/span')
+        mingxichaxun_h2 = self.driver.find_element_by_xpath('//span[text()="明细查询"]')
 
         shishijiance_h1.click()
         shishigongkuang_h2.click()
-        word_element_4 = '//*[@id="svg_background_svg_name"]/svg/rect'
+
+        # 检查有没有出现弹窗提醒，有则点确定关掉弹窗
+        time.sleep(3)
+        try:
+            ok = self.driver.switch_to.alert
+            ok.accept()
+        except e.NoAlertPresentException as noalert:
+            print(noalert)
+
+        # 切换站点到 宜山大楼
+        xiala_element = self.driver.find_element_by_xpath('//*[@id="app"]/div/div/div[2]/div[2]/div/div/div[1]/div[1]/div/div/span/span/i')
+        xiala_element.click()
+        yishandalou = self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/ul/li[4]/span')
+        yishandalou.click()
+
+        time.sleep(2)
+        word_element_4 = '//*[@id="text4"]/text'
         try:
             WebDriverWait(self.driver, 5, 0.5).until(expected_conditions.visibility_of_element_located((By.XPATH, word_element_4)))
-        except e.InvalidSelectorException as error:
-            print("元素定位表达式 logo 不正确，请修正：{}".format(word_element_1))
-            self.logger.error("元素定位表达式 logo 不正确，请修正：{}".format(word_element_1))
-        except e.NoSuchElementException as error:
-            print("没有找到该元素")
-            self.logger.error("没有找到该元素")
-        except TimeoutError as error:
-            print("等待页面超时")
+        # except e.InvalidSelectorException as error:
+        #     print("元素定位表达式 logo 不正确，请修正：{}".format(word_element_1))
+        #     self.logger.error("元素定位表达式 logo 不正确，请修正：{}".format(word_element_1))
+        # except e.NoSuchElementException as error:
+        #     print("没有找到该元素")
+        #     self.logger.error("没有找到该元素")
+        # except TimeoutError as error:
+        #     print("等待页面超时")
+        except Exception as error:
+            self.logger.error(error)
+            print(error)
 
         mingxichaxun_h2.click()
         word_element_5 = '//*[@id="pane-day"]/button'
